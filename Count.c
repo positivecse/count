33c029608a107e4c869ae5aaf498767dc9bff93f
positivecse <47621096+positivecse@users.noreply.github.com>
2019-08-11 18:06:37 +0530

    Create Count.c

---
+++ Count.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int n,count=0;
+clrscr();
+printf("Enter any number:");
+scanf("%d",&n);
+while(n>0)
+{
+count++;
+n=n/10;
+}
+printf("Number of digits:%d",count);
+getch();
+}
